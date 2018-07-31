3080bbdb54be8fab4aa0020fe61f4a7f3a3c3cd0
Luke Bousfield <tech.master.champ@gmail.com>
2018-07-31 14:30:05 -0600

    Renamed variables and better complied with best practices

--- main.py
+++ main.py
@@ -3,42 +3,34 @@ from flask import Flask, render_template, request, redirect
 #from gpiozero import LED
 import os
 
+global isPowered
 
-global status
-global statusval
-global buttonval
-buttonval = "Turn off"
-status = 1
+isPowered = True
+
 app = Flask(__name__)
 
 dir = os.getcwd()
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-	global status, statusval, buttonval
-	if status == 1:
-		statusval = "Status: On"
-
-	else: statusval = "Status: off"
-	return render_template('index.html', name = statusval, tname = buttonval)
+	return render_template('index.html', isPowered = isPowered)
 
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
-	global status, statusval, buttonval
-	if status == 1:
-		#turn gpio pin on
-		statusval = "Status: Off"
-		status = 0
-		buttonval = "Turn on"
-		return redirect("/", code=302)
 
-	if status == 0:
-		#turn gpio pin off
-		statusval = "Status: On"
-		status = 1
-		buttonval = "Turn off"
-		return redirect("/", code=302)
+	global isPowered
+	isPowered = not isPowered
+
+	if isPowered:
+		print("Turning device on")
+		# turn gpio pin off, turning device on
+	else:
+		print("Turning device off")
+		# turn gpio pin on, turning device off
+		
+	return redirect("/", code = 302)
+	
 
 if __name__  == "__main__":
 	app.run(debug=True)
